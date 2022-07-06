214a73ec2b943be849425d8e39b54dd1b9bac516
JoonSang Yoo <js960913@naver.com>
2022-07-06 12:14:58 +0900

    2022.07.06 - tours  수정사항[팀장]

---
+++ BoardStudyyy/src/main/java/kr/co/vo/Arrive.java
@@ -0,0 +1,43 @@
+package kr.co.vo;
+
+public class Arrive {
+	
+	private String arr_id;
+	private String arr_loadaddress;
+	private String arr_gnumaddress;
+	private double arr_ny;
+	private double arr_nx;
+	public String getArr_id() {
+		return arr_id;
+	}
+	public void setArr_id(String arr_id) {
+		this.arr_id = arr_id;
+	}
+	public String getArr_loadaddress() {
+		return arr_loadaddress;
+	}
+	public void setArr_loadaddress(String arr_loadaddress) {
+		this.arr_loadaddress = arr_loadaddress;
+	}
+	public String getArr_gnumaddress() {
+		return arr_gnumaddress;
+	}
+	public void setArr_gnumaddress(String arr_gnumaddress) {
+		this.arr_gnumaddress = arr_gnumaddress;
+	}
+	public double getArr_ny() {
+		return arr_ny;
+	}
+	public void setArr_ny(double arr_ny) {
+		this.arr_ny = arr_ny;
+	}
+	public double getArr_nx() {
+		return arr_nx;
+	}
+	public void setArr_nx(double arr_nx) {
+		this.arr_nx = arr_nx;
+	}
+	
+	
+
+}
